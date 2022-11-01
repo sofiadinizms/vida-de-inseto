e7ce6b64d55be0faf957283498f993b95a2e6330
sofiadinizms <sofiadinizms@MacBook-Pro-64.local>
2022-11-01 15:58:27 -0300

    fix(feature/leve-3-2): transferred everything from branch feature/level-3 to here

---
+++ VidaDeInseto/VidaDeInseto/LevelThreeView.swift
@@ -0,0 +1,47 @@
+//
+//  LevelThreeView.swift
+//  VidaDeInseto
+//
+//  Created by sofiadinizms on 01/11/22.
+//
+
+import SwiftUI
+
+struct LevelThreeView: View {
+    @State var brightness = UIScreen.main.brightness
+    @State var night = false
+    @Environment(\.scenePhase) var scenePhase
+    
+    var body: some View {
+        VStack{
+            if night{
+                Image("moon")
+            } else {
+                Image("sun")
+            }
+            Image("sad-mushroom")
+        }.onChange(of: scenePhase) { newPhase in
+            if (newPhase == .active) {
+                print("Active -- \(UIScreen.main.brightness)")
+                if UIScreen.main.brightness == CGFloat(0){
+                    print("ganhou!")
+                    night = true
+                }
+                
+            } else if newPhase == .inactive{
+                print("Inactive")
+                
+            } else if (newPhase == .background) {
+                print("Background")
+            }
+            
+        }
+    }
+}
+
+
+struct LevelThreeView_Previews: PreviewProvider {
+    static var previews: some View {
+        LevelThreeView()
+    }
+}
